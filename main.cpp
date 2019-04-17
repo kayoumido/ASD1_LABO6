54a2deb3c0785ade3e80923a2f4b5b0093229f09
gab-thr <gabrielle.thurnherr@gmail.com>
2019-04-17 14:15:25 +0200

    mise en page jolie

--- main.cpp
+++ main.cpp
@@ -6,14 +6,17 @@ using namespace std;
 int main() {
 
     // DC IC(5) CC(5) MC(5) C=(5) M=(5) D(-2) D(-1) D(5) D(5)
+
+/**  CODECHECK 1
+     Int p2 = 5;         // IC
+     Int p3 = p2;        // CC
+     Int p4 = move(p3);  // MC
+     p1 = p2;            // C=
+     p1 = move(p4);      // M=
+ */
+
     // DC IC(5) CC(5) MC(5) C=(5) M=(5) D(-2) D(-1) D(5) D(5)
 
-    Int p1;             // DC
-    Int p2 = 5;         // IC
-    Int p3 = p2;        // CC
-    Int p4 = move(p3);  // MC
-    p1 = p2;            // C=
-    p1 = move(p4);      // M=
 
     return 0;
 }
