a1f4e9b21064403a257d0be3a1237f47453789b7
Doran <dorankayoumi@gmail.com>
2019-04-17 13:48:26 +0200

    Test stuff

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include "Mouchard.h"
+
+using namespace std;
 
 int main() {
 
+    // DC IC(5) CC(5) MC(5) C=(5) M=(5) D(-2) D(-1) D(5) D(5)
 
+    Int p(5);
+    p = 5;
+
+    Int p2 = p;
     return 0;
 }
