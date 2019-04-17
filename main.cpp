79eec51d7aa3529de0fb96bb2c78f68cd825ddc8
Doran <dorankayoumi@gmail.com>
2019-04-17 14:10:01 +0200

    Valid 1st codecheck

--- main.cpp
+++ main.cpp
@@ -6,10 +6,14 @@ using namespace std;
 int main() {
 
     // DC IC(5) CC(5) MC(5) C=(5) M=(5) D(-2) D(-1) D(5) D(5)
+    // DC IC(5) CC(5) MC(5) C=(5) M=(5) D(-2) D(-1) D(5) D(5)
 
-    Int p(5);
-    p = 5;
+    Int p1;             // DC
+    Int p2 = 5;         // IC
+    Int p3 = p2;        // CC
+    Int p4 = move(p3);  // MC
+    p1 = p2;            // C=
+    p1 = move(p4);      // M=
 
-    Int p2 = p;
     return 0;
 }
