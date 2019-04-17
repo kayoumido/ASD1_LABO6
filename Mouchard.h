7e7aecf67ee4bade44860b004f5bf60dc5fc0f1b
Doran <dorankayoumi@gmail.com>
2019-04-17 13:24:35 +0200

    Add inital files for 1st codecheck

---
+++ Mouchard.h
@@ -0,0 +1,29 @@
+//
+// Created by deekay on 17/04/19.
+//
+
+#ifndef LABO6_MOUCHARD_H
+#define LABO6_MOUCHARD_H
+
+#include <ostream>
+
+class Int;
+
+std::ostream& operator<<( std::ostream& os, const Int& i);
+
+class Int {
+    friend std::ostream& operator<<( std::ostream& os, const Int& i);
+    int data;
+    int checkConstruction;
+    static const int OK = 20180413;
+public:
+    Int();
+    ~Int();
+    Int(int d);
+    Int(const Int& other);
+    Int(Int&& other);
+    Int& operator= (const Int& other);
+    Int& operator= (Int&& other);
+};
+
+#endif //LABO6_MOUCHARD_H
