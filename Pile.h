d6447693aaacef1ba281f3548ad2b33a79cb4223
Doran <dorankayoumi@gmail.com>
2019-04-23 11:48:15 +0200

    Change code to pass 5th codecheck

--- Pile.h
+++ Pile.h
@@ -11,7 +11,8 @@ template<typename T>
 std::ostream &operator<<(std::ostream &os,
                          const Pile<T> &p);
 
-class PileOverflowException : public std::exception {};
+class PileOverflowException : public std::exception {
+};
 
 // Type Pile
 template<typename T>
@@ -31,18 +32,23 @@ private:
     const size_t CAPACITE;
 
 public:
-    explicit Pile(size_t _capacite) : CAPACITE(_capacite), donnees(new T[_capacite]), taille(0) {}
+    explicit Pile(size_t _capacite) : CAPACITE(_capacite), donnees((T *) ::operator new(_capacite * sizeof(T))),
+                                      taille(0) {}
 
     ~Pile() {
-        // a completer
-        delete[] donnees;
+
+        for (size_t i = 0; i < this->taille; ++i)
+            (this->donnees + i)->~T();
+
+        ::operator delete(this->donnees);
     }
 
     Pile(const Pile &other) : CAPACITE(other.CAPACITE), donnees(nullptr), taille(0) {
-        this->donnees = new T[this->CAPACITE];
+
+        this->donnees = (T *) ::operator new(this->CAPACITE * sizeof(T));
 
         for (size_t i = 0; i < other.taille; ++i) {
-            *(this->donnees + i) = *(other.donnees + i);
+            new(this->donnees + i) T(*(other.donnees + i));
             ++this->taille;
         }
     }
@@ -50,12 +56,13 @@ public:
     void empiler(const value_type &v) {
         if (this->taille == this->CAPACITE) throw PileOverflowException();
 
-        *(donnees + taille) = v;
+        new(donnees + taille) T(v);
         ++taille;
     }
 
     void depiler() {
         --taille;
+        (this->donnees + taille)->~T();
     }
 
     const_reference sommet() const {
